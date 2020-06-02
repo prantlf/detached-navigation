058780a8957c38fc4532f3611c05da3dfdb46b56
Ferdinand Prantl <prantlf@gmail.com>
2020-06-02 22:36:20 +0200

    chore: Simplify code

--- test/detach-backbone.js
+++ test/detach-backbone.js
@@ -2,12 +2,12 @@ import createDetachedBrowser from '../lib/detached-browser'
 import detachBackboneHistory from '../lib/detach-backbone'
 
 exports['test detach-backbone: re-attaches the location and history'] = assert => {
-  const Backbone = { history: {} }
-  const undo = detachBackboneHistory(Backbone.history, createDetachedBrowser())
+  const history = {}
+  const undo = detachBackboneHistory(history, createDetachedBrowser())
   assert.equal(typeof undo, 'function', 'returns an undoing function')
   undo()
-  assert.equal(Backbone.history.location, undefined, 're-attaches original location')
-  assert.equal(Backbone.history.history, undefined, 're-attaches original history')
+  assert.equal(history.location, undefined, 're-attaches original location')
+  assert.equal(history.history, undefined, 're-attaches original history')
 }
 
 if (require.main === module) { require('test').run(exports) }
